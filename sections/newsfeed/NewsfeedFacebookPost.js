8036d1267b089d92bc477357fc7da02da0588aac
unus <yali71507@gmail.com>
2024-09-14 17:10:36 +0600

    facebook post updated

--- sections/newsfeed/NewsfeedFacebookPost.js
+++ sections/newsfeed/NewsfeedFacebookPost.js
@@ -44,10 +44,10 @@ function NewsfeedFacebookPost() {
             <Box>
               <iframe
                 src={`https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAbroadinquiry&tabs=timeline&width=${
-                  matchesMd ? "100%" : "100%"
+                  matchesMd ? "100%" : "500"
                 }&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId`}
                 width="100%"
-                height="700"
+                height="500"
                 style={{
                   border: "none",
                   overflow: "hidden",
