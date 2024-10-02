48ae77d2574fa8f2df09ed7b087daf10fec464fe
chistyhimel <fc_himel@yahoo.com>
2024-10-03 00:11:01 +0600

    metadata updated

--- app/(main)/newsfeed/[slug]/page.js
+++ app/(main)/newsfeed/[slug]/page.js
@@ -1,67 +1,39 @@
-"use client";
-import { fetchBlogPostDetails } from "@/redux/features/newsfeed/newsfeedSlice";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootStyle } from "../../contact/style";
-import NewsfeedDetails from "@/sections/newsfeed/NewsfeedDetails";
-import {
-  Box,
-  Container,
-  useMediaQuery,
-  CircularProgress,
-  useTheme,
-} from "@mui/material";
-import { useParams } from "next/navigation";
-import NewsfeedFacebookPost from "@/sections/newsfeed/NewsfeedFacebookPost";
+import { getblogPostDetails } from "@/redux/features/newsfeed/newsfeedAPI";
+import NewsfeedSinglePost from "@/sections/newsfeed/NewsfeedSinglPost";
+import { BASE_URL } from "@/utils/axios";
 
-const BlogPostDetails = () => {
-  const theme = useTheme();
-  const dispatch = useDispatch();
-  const matchSm = useMediaQuery("(min-width:600px)");
-  const params = useParams();
+function stripHtml(html) {
+  return html.replace(/<[^>]+>/g, "");
+}
+
+export async function generateMetadata({ params }) {
+  const blog = await getblogPostDetails(params.slug);
+
+  const defaultDescription = "Read the latest article from Abroad Inquiry.";
+
+  const description = blog?.data?.blogBody
+    ? stripHtml(blog.data.blogBody).slice(0, 160) + "..."
+    : defaultDescription;
+
+  return {
+    title: blog?.data?.blogTitle || "Blog Post",
+    description: description,
+    openGraph: {
+      title: blog?.data?.blogTitle || "Blog Post",
+      description: description,
+      images: [
+        {
+          url: `${BASE_URL}/${blog?.data?.blogFeaturedImage}`,
+        },
+      ],
+    },
+  };
+}
+
+const BlogPostDetails = ({ params }) => {
   const { slug } = params;
-  const { blogDetails, loading } = useSelector((state) => state.blogs);
 
-  useEffect(() => {
-   if(slug){
-    dispatch(fetchBlogPostDetails(slug));
-   }
-  }, [dispatch,slug]);
-
-  return (
-    <RootStyle>
-      {loading ? (
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-          }}
-        >
-          <CircularProgress
-            sx={{ color: theme.palette.mode === "dark" ? "white" : "black" }}
-          />
-        </Box>
-      ) : (
-        <Container
-          style={{
-            display: "flex",
-            marginTop: "50px",
-            flexDirection: matchSm ? "row" : "column",
-            justifyContent: "space-between",
-          }}
-        >
-          <Box sx={{ width: matchSm ? "70%" : "100%", marginRight: matchSm ? 2 : 0 }}>
-            <NewsfeedDetails blogDetails={blogDetails} />
-          </Box>
-          <Box sx={{ width: matchSm ? "30%" : "100%",}}>
-            <NewsfeedFacebookPost />
-          </Box>
-        </Container>
-      )}
-    </RootStyle>
-  );
+  return <NewsfeedSinglePost blogId={slug} />;
 };
 
 export default BlogPostDetails;
--- redux/features/newsfeed/newsfeedSlice.js
+++ redux/features/newsfeed/newsfeedSlice.js
@@ -1,68 +1,70 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import { getAllblogPosts, getblogPostDetails } from "./newsfeedAPI.js"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAllblogPosts, getblogPostDetails } from "./newsfeedAPI.js";
 
 const initialState = {
-    loading: false,
-    isError: false,
-    error: "",
-    blogs: [],
-    blogDetails: {}
-}
+  loading: false,
+  isError: false,
+  error: "",
+  blogs: [],
+  blogDetails: {},
+};
 
+//create thunk
 
-//create thunk 
-
-export const fetchBlogPosts = createAsyncThunk("blogPosts/fetchBlogPosts", async (page) => {
+export const fetchBlogPosts = createAsyncThunk(
+  "blogPosts/fetchBlogPosts",
+  async (page) => {
     const blogs = getAllblogPosts(page);
     return blogs;
-})
+  }
+);
 
-export const fetchBlogPostDetails = createAsyncThunk("blogPosts/fetchBlogPostDetails", async (id) => {
+export const fetchBlogPostDetails = createAsyncThunk(
+  "blogPosts/fetchBlogPostDetails",
+  async (id) => {
     const blogDetails = getblogPostDetails(id);
     return blogDetails;
-})
+  }
+);
 
-
-// create slice 
+// create slice
 
 const blogPostsSlice = createSlice({
-    name: "blogPosts",
-    initialState,
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchBlogPosts.pending, (state) => {
-                state.loading = true;
-                state.isError = false;
-            })
-            .addCase(fetchBlogPosts.fulfilled, (state, action) => {
-                state.loading = false;
-                state.isError = false;
-                state.blogs = action.payload.data.blogs;
-            })
-            .addCase(fetchBlogPosts.rejected, (state, action) => {
-                state.loading = false;
-                state.isError = true;
-                state.error = action.error?.message;
-                state.blogs = []
-            })
-            .addCase(fetchBlogPostDetails.pending, (state) => {
-                state.loading = true;
-                state.isError = false;
-            })
-            .addCase(fetchBlogPostDetails.fulfilled, (state, action) => {
-                state.loading = false;
-                state.isError = false;
-                state.blogDetails = action.payload.data;
-            })
-            .addCase(fetchBlogPostDetails.rejected, (state, action) => {
-                state.loading = false;
-                state.isError = true;
-                state.error = action.error?.message;
-                state.blogDetails = {}
-            })
-    }
-})
-
-
+  name: "blogPosts",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBlogPosts.pending, (state) => {
+        state.loading = true;
+        state.isError = false;
+      })
+      .addCase(fetchBlogPosts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.isError = false;
+        state.blogs = action.payload.data.blogs;
+      })
+      .addCase(fetchBlogPosts.rejected, (state, action) => {
+        state.loading = false;
+        state.isError = true;
+        state.error = action.error?.message;
+        state.blogs = [];
+      })
+      .addCase(fetchBlogPostDetails.pending, (state) => {
+        state.loading = true;
+        state.isError = false;
+      })
+      .addCase(fetchBlogPostDetails.fulfilled, (state, action) => {
+        state.loading = false;
+        state.isError = false;
+        state.blogDetails = action.payload.data;
+      })
+      .addCase(fetchBlogPostDetails.rejected, (state, action) => {
+        state.loading = false;
+        state.isError = true;
+        state.error = action.error?.message;
+        state.blogDetails = {};
+      });
+  },
+});
 
 export default blogPostsSlice.reducer;
